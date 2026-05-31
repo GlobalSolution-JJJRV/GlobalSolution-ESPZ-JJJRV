c8d74a1763e72516257fe26831e4ab78ddbdb382
Boletinirafs <rafa.bo.oliv@gmail.com>
2026-05-31 14:38:27 -0300

    Adicionado o modo Galaxy para completar 3 modos requisitados

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+
+//Modifica os temas da página
 const lightModes = document.getElementById('light-dark')
 lightModes.addEventListener('click',()=>{
     const currentMode = lightModes.getAttribute('alt')
     const modeSwitch = document.getElementById('mode')
-    if (currentMode == 'darkMode'){
+    if (currentMode == 'darkMode'){ //Dark para light
         lightModes.setAttribute('alt','lightMode')
         lightModes.setAttribute('src','images/light-dark/lightMode.svg')
         modeSwitch.setAttribute('href','css/lightStyles.css')
 
-    }else{
+    }else if(currentMode == 'lightMode'){ //Light para galaxy
+        lightModes.setAttribute('alt','galaxyMode')
+        lightModes.setAttribute('src','images/light-dark/galaxyMode.svg')
+        modeSwitch.setAttribute('href','css/galaxyStyles.css')
+    }else{ //Galaxy para dark
         lightModes.setAttribute('alt','darkMode')
         lightModes.setAttribute('src','images/light-dark/darkMode.svg')
         modeSwitch.setAttribute('href','css/darkStyles.css')
